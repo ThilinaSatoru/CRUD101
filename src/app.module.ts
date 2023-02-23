6545b08d9220e95ed52174634b001c020858610d
thilina-cosmoforge <thilina@cosmoforge.com>
2023-02-23 10:54:53 +0530

    www.thisdot.co - restful-apis-with-nestjs.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './entities/users/user.module';
+import { PostsService } from './posts/posts.service';
+import { PostsController } from './posts/posts.controller';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PostsController],
+  providers: [AppService, PostsService],
+  imports: [PostsModule],
 })
 export class AppModule {}
--- src/entities/users/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { UserController } from "./users.controller";
-import { Module } from "@nestjs/common/decorators";
-import { UsersService } from "./users.service";
-
-@Module({
-    controllers: [UserController],
-    providers: [UsersService],
-})
-export class UserModule {}
