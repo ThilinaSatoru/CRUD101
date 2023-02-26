983f09f0f7819bec1fb932d945feffb5cd740450
ThilinaSatoru <satoru.thilina@gmail.com>
2023-02-26 16:34:27 +0530

    JWT intergration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './entities/users/user.module';
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [UserModule],
+  imports: [UsersModule,],
   controllers: [AppController],
   providers: [AppService],
 })
